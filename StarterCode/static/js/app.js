640d7a989be0571ca300fed6a1d8ff4b02411ace
TomasCF <tomas.cousillas@nyu.edu>
2019-11-21 13:26:50 -0500

    remove blank rows edit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,43 @@
+// YOUR CODE HERE!
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the UFO data from data.js
+//console.log(data);
+
+// Step 1: Loop Through `data` and console.log each UFO report object
+data.forEach(function(UFOReport) {
+    console.log(UFOReport);
+  });
+ 
+ // // Step 2:  Use d3 to append one table row `tr` for each UFO report object
+ // // Don't worry about adding cells or text yet, just try appending the `tr` elements.
+  data.forEach(function(UFOReport) {
+    console.log(UFOReport);
+    var row = tbody.append("tr");
+  });
+ 
+  // Step 3:  Use `Object.entries` to console.log each UFO report value
+  data.forEach(function(UFOReport) {
+    console.log(UFOReport);
+    var row = tbody.append("tr");
+ 
+    Object.entries(UFOReport).forEach(function([key, value]) {
+      console.log(key, value);
+    });
+  });
+
+ // // Step 4: Use d3 to update each cell's text with UFO report values 
+  data.forEach(function(UFOReport) {
+    console.log(UFOReport);
+    var row = tbody.append("tr");
+    Object.entries(UFOReport).forEach(function([key, value]) {
+     console.log(key, value);
+ // // Append a cell to the row for each value in the UFO report object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
